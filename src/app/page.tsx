078c830b8f0d85220fafac4d9b858d65ace82755
JeehyaeDance <jeehyae91@gmail.com>
2024-01-27 22:14:08 +0900

    delete console log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import {db} from "../lib/drizzle"
 import { sql } from 'drizzle-orm' 
 
 export default async function Home() {
-  console.log(await db.execute(sql`select now();`))
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
